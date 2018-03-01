9fcceacca33466fb91cfc9feacb6466b6c4587ed
TheKlemi2 <theklemi2@hotmail.fr>
2018-03-01 09:35:03 +0100

    update

--- TP1/ex1.php
+++ TP1/ex1.php
@@ -5,10 +5,28 @@
         <title>Hello</title>
     </head>
 <body>
-
+    
     <?php
         echo("Hello world !");
     ?>
+
+<?php
+echo("<h1>Salut ");
+if(isset($_GET["message"])){
+	echo $_GET["message"];
+}
+echo(" !</h1>");
+?>
+    
+        
+    <br>
+    <a href="ex1.php?message=toto">Afficher toto</a>
+    <br>
+    <a href="ex1.php?message=titi">Afficher titi</a>
+    <br>
+    <a href="ex1.php">Afficher salut</a>
+    
+
 </body>
     
 </html>
