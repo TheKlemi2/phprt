72d9bb8bbea79740c67bb694cef4b128f8672d0e
TheKlemi2 <theklemi2@hotmail.fr>
2018-03-01 08:58:19 +0100

    Ex 0

--- TP1/ex1.php
+++ TP1/ex1.php
@@ -1,1 +1,14 @@
-hello world
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Hello</title>
+    </head>
+<body>
+
+    <?php
+        echo("Hello world !");
+    ?>
+</body>
+    
+</html>
